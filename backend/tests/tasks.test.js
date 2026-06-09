9a0eddb7545e2a75f643660f5aaeb865b1ab4bb9
kit2422mmc061-lgtm <kit2422mmc061@gmail.com>
2026-06-09 11:31:03 +0530

    initial commit

---
+++ backend/tests/tasks.test.js
@@ -0,0 +1,24 @@
+const request = require("supertest");
+const { server, app } = require("../index");
+const moongose = require('mongoose')
+
+describe('GET api/tasks', ()=>{
+
+    it('it should return 200 ok',async()=>{
+        const res = await request(app).get('/api/tasks')
+        expect(res.statusCode).toBe(200)
+    })
+    
+    it('it should return array ',async()=>{
+        const res = await request(app).get('/api/tasks')
+        expect(Array.isArray(res.body)).toBe(true)
+    })
+
+
+})
+
+
+afterAll(async()=>{
+    await moongose.connection.close()
+    await server.close()
+})
